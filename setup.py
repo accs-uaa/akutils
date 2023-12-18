bbb78bf17534073f8d09f4da145438c7cb89743b
Timm Nawrocki <twnawrocki@alaska.edu>
2023-12-18 11:43:24 -0900

    Update version to 0.3.2
    Updated version to 0.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='akutils',
-    version='0.3.1',
+    version='0.3.2',
     author='Timm Nawrocki',
     author_email='twnawrocki@alaska.edu',
     description='Functions and utilities for Alaska geospatial data development.',
