cde92acc095f8bc88c4fc64ca8d06a1d6e6198e2
Timm Nawrocki <twnawrocki@alaska.edu>
2024-11-24 21:57:27 -0900

    Add classifier cross val
    Added a custom cross validation function for the classifier that accounts for the threshold assignment.

--- akutils/optimization_lgbm.py
+++ akutils/optimization_lgbm.py
@@ -7,6 +7,103 @@
 # Description: "Optimization for LightGBM" is a set of functions that perform Bayesian optimization on either a LightGBM classifier or regressor.
 # ---------------------------------------------------------------------------
 
+# Define a function to calculate the cross validated balanced accuracy score for the classifier
+def cross_val_bacc_classifier(estimator, data, all_variables, predictor_all, target_field, stratify_field, group_field):
+    # Import packages
+    import numpy as np
+    import pandas as pd
+    from sklearn.model_selection import StratifiedGroupKFold
+    from sklearn.metrics import balanced_accuracy_score
+    from akutils import determine_optimal_threshold
+
+    # Create inner cv splits
+    inner_cv_splits = StratifiedGroupKFold(n_splits=5)
+    inner_split = ['inner_split_n']
+
+    # Create an empty data frame to store the inner cross validation splits
+    inner_train = pd.DataFrame(columns=all_variables + inner_split)
+    inner_test = pd.DataFrame(columns=all_variables + inner_split)
+
+    # Create an empty data frame to store the inner test results
+    inner_results = pd.DataFrame(columns=all_variables + inner_split + ['y_abs', 'y_pres'])
+
+    # Create inner cross validation splits
+    count = 1
+    for train_index, test_index in inner_cv_splits.split(data,
+                                                         data[stratify_field[0]].astype('int32'),
+                                                         data[group_field[0]].astype('int32')):
+        # Split the data into train and test partitions
+        train = data.iloc[train_index]
+        test = data.iloc[test_index]
+        # Insert iteration to train
+        train = train.assign(inner_split_n=count)
+        # Insert iteration to test
+        test = test.assign(inner_split_n=count)
+        # Append to data frames
+        inner_train = pd.concat([inner_train if not inner_train.empty else None,
+                                 train],
+                                axis=0)
+        inner_test = pd.concat([inner_test if not inner_test.empty else None,
+                                test],
+                               axis=0)
+        # Increase counter
+        count += 1
+    inner_cv_length = count - 1
+
+    # Reset indices
+    inner_train = inner_train.reset_index()
+    inner_test = inner_test.reset_index()
+
+    # Iterate through inner cross validation splits
+    inner_cv_i = 1
+    while inner_cv_i <= inner_cv_length:
+        inner_train_iteration = inner_train[inner_train[inner_split[0]] == inner_cv_i].copy()
+        inner_test_iteration = inner_test[inner_test[inner_split[0]] == inner_cv_i].copy()
+
+        # Identify X and y inner train and test splits
+        X_class_inner = inner_train_iteration[predictor_all].astype(float).copy()
+        y_class_inner = inner_train_iteration[target_field[0]].astype('int32').copy()
+        X_test_inner = inner_test_iteration[predictor_all].astype(float).copy()
+
+        # Train regressor on the inner train data
+        estimator.fit(X_class_inner, y_class_inner)
+
+        # Predict inner test data
+        probability_inner = estimator.predict_proba(X_test_inner)
+
+        # Assign predicted values to inner test data frame
+        inner_test_iteration = inner_test_iteration.assign(y_abs=probability_inner[:, 0])
+        inner_test_iteration = inner_test_iteration.assign(y_pres=probability_inner[:, 1])
+
+        # Add the test results to output data frame
+        inner_results = pd.concat([inner_results if not inner_results.empty else None,
+                                   inner_test_iteration],
+                                  axis=0)
+
+        # Increase n value
+        inner_cv_i += 1
+
+    # Calculate the optimal threshold and performance of the presence-absence classification
+    threshold, sensitivity, specificity, auc, accuracy = determine_optimal_threshold(
+        inner_results['y_pres'],
+        inner_results[target_field[0]]
+    )
+
+    # Convert probability to presence-absence
+    presence_zeros = np.zeros(inner_results['y_pres'].shape)
+    presence_zeros[inner_results['y_pres'] >= threshold] = 1
+
+    # Assign binary prediction values to test data frame
+    inner_results = inner_results.assign(y_pred=presence_zeros)
+
+    # Calculate negative mean squared error
+    y_class_observed = inner_results[target_field[0]].astype('int32').copy()
+    y_class_predicted = inner_results['y_pred'].astype('int32').copy()
+    bacc = balanced_accuracy_score(y_class_observed, y_class_predicted)
+
+    return bacc
+
+
 # Define a function to calculate the cross validated negative mean squared error for the regressor
 def cross_val_nmse_regressor(estimator, data, all_variables, predictor_all, target_field, stratify_field, group_field):
     # Import packages
@@ -26,7 +123,7 @@ def cross_val_nmse_regressor(estimator, data, all_variables, predictor_all, targ
     inner_test = pd.DataFrame(columns=all_variables + inner_split)
 
     # Create an empty data frame to store the inner test results
-    inner_results = pd.DataFrame(columns=all_variables + inner_split + ['y_predicted'])
+    inner_results = pd.DataFrame(columns=all_variables + inner_split + ['y_pred'])
 
     # Create inner cross validation splits
     count = 1
@@ -63,7 +160,7 @@ def cross_val_nmse_regressor(estimator, data, all_variables, predictor_all, targ
 
         # Identify X and y inner train and test splits
         X_regress_inner = inner_train_iteration[predictor_all].astype(float).copy()
-        y_regress_inner = inner_train_iteration[target_field[0]].astype('int32').copy()
+        y_regress_inner = inner_train_iteration[target_field[0]].astype(float).copy()
         X_test_inner = inner_test_iteration[predictor_all].astype(float).copy()
 
         # Train regressor on the inner train data
@@ -73,7 +170,7 @@ def cross_val_nmse_regressor(estimator, data, all_variables, predictor_all, targ
         prediction_inner = estimator.predict(X_test_inner)
 
         # Assign predicted values to inner test data frame
-        inner_test_iteration = inner_test_iteration.assign(y_predicted=prediction_inner)
+        inner_test_iteration = inner_test_iteration.assign(y_pred=prediction_inner)
 
         # Add the test results to output data frame
         inner_results = pd.concat([inner_results if not inner_results.empty else None,
@@ -85,16 +182,17 @@ def cross_val_nmse_regressor(estimator, data, all_variables, predictor_all, targ
 
     # Calculate negative mean squared error
     y_regress_observed = inner_results[target_field[0]].astype(float).copy()
-    y_regress_predicted = inner_results['y_predicted'].astype(float).copy()
+    y_regress_predicted = inner_results['y_pred'].astype(float).copy()
     nmse = -(mean_squared_error(y_regress_observed, y_regress_predicted))
 
     return nmse
+
 
 # Define a function to conduct a cross validation iteration for a LightGBM classifier
 def lgbmclassifier_cv(num_leaves, max_depth, learning_rate, n_estimators,
                       min_split_gain, min_child_weight, min_child_samples,
                       subsample, colsample_bytree, reg_alpha, reg_lambda,
-                      data, targets, groups):
+                      data, all_variables, predictor_all, target_field, stratify_field, group_field):
     """
     Description: conducts cross validation of a LightGBM regressor with a particular set of hyperparameter values
     Inputs: 'data' -- the covariate data to conduct the model training and validation
@@ -108,7 +206,6 @@ def lgbmclassifier_cv(num_leaves, max_depth, learning_rate, n_estimators,
     # Import packages
     from lightgbm import LGBMClassifier
     from sklearn.model_selection import StratifiedGroupKFold
-    from sklearn.model_selection import cross_val_score
 
     # Define cross validation
     cv_splits = StratifiedGroupKFold(n_splits=5, shuffle=True)
@@ -136,16 +233,15 @@ def lgbmclassifier_cv(num_leaves, max_depth, learning_rate, n_estimators,
     )
 
     # Define cross validation
-    scores = cross_val_score(
-        estimator,
-        data,
-        targets,
-        scoring='balanced_accuracy',
-        cv=cv_splits,
-        groups=groups
-    )
+    bacc = cross_val_bacc_classifier(estimator,
+                                     data,
+                                     all_variables,
+                                     predictor_all,
+                                     target_field,
+                                     stratify_field,
+                                     group_field)
 
-    return scores.mean()
+    return bacc
 
 
 # Define a function to conduct a cross validation iteration for a LightGBM regressor
@@ -201,7 +297,7 @@ def lgbmregressor_cv(num_leaves, max_depth, learning_rate, n_estimators,
 
 
 # Define a function to optimize hyperparameters for a LightGBM classifier
-def optimize_lgbmclassifier(init_points, n_iter, data, targets, groups):
+def optimize_lgbmclassifier(init_points, n_iter, data, all_variables, predictor_all, target_field, stratify_field, group_field):
     """
     Description: applies Bayesian optimization to the hyperparameters of a LightGBM classifier
     Inputs: 'data' -- the covariate data to conduct the model training and validation
@@ -240,8 +336,11 @@ def optimize_lgbmclassifier(init_points, n_iter, data, targets, groups):
             reg_alpha=reg_alpha,
             reg_lambda=reg_lambda,
             data=data,
-            targets=targets,
-            groups=groups
+            all_variables=all_variables,
+            predictor_all=predictor_all,
+            target_field=target_field,
+            stratify_field=stratify_field,
+            group_field=group_field
         )
 
     optimizer = BayesianOptimization(
@@ -268,7 +367,8 @@ def optimize_lgbmclassifier(init_points, n_iter, data, targets, groups):
 
 
 # Define a function to optimize hyperparameters for a LightGBM regressor
-def optimize_lgbmregressor(init_points, n_iter, data, all_variables, predictor_all, target_field, stratify_field, group_field):
+def optimize_lgbmregressor(init_points, n_iter, data, all_variables, predictor_all, target_field, stratify_field,
+                           group_field):
     """
     Description: applies Bayesian optimization to the hyperparameters of a LightGBM regressor
     Inputs: 'data' -- the covariate data to conduct the model training and validation
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='akutils',
-    version='0.7.1',
+    version='0.7.2',
     author='Timm Nawrocki',
     author_email='twnawrocki@alaska.edu',
     description='Functions and utilities for Alaska geospatial data development.',
